b9baccb41e60eb1157934ade2cb8852886538448
Aryan Singh <aryansingh532006@gmail.com>
2025-08-01 22:25:32 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,6 @@ const Navbar = () => {
     <nav className='text-white bg-black-900-200 h-13 flex justify-between items-center px-8 py-3'>
         <img src={logo} alt="ENCRYPTO" className='logo h-10 rounded-xl m-2 border-2 border-indigo-300'/>
         <ul>
-            <li className='flex gap-8 text-xl m-2'>
-                <a className='hover:font-bold hover:underline text-indigo-300 duration-100' href='#'>Login</a>
-            </li>
         </ul>
     </nav>
   )
